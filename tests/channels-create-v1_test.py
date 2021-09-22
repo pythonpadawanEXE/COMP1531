7d733587efe6c197439cbed5fcc2c2be04dcee45
code-monke <50727669+code-monke@users.noreply.github.com>
2021-09-22 15:08:03 +1000

    Added tests for InputError cases (name too long/short)

--- tests/channels-create-v1_test.py
+++ tests/channels-create-v1_test.py
@@ -1,8 +1,36 @@
-# channels-create_test.py
+# channels-create-v1_test.py
 # pytest file to test the implementation of channels_create_v1
 
 import pytest
 
-def test_channels_create_v1_simple():
-    new_channel = channels_create_v1()
+from src.channels import channels_create_v1
+from src.error import InputError
+
+@pytest.fixture
+def chan1():
+    return (1, 'My Channel', True)
+
+@pytest.fixture
+def invalid_chan1():
+    return (1, '', True)
+
+@pytest.fixture
+def invalid_chan2():
+    return (1, 'AAAAAAAAAAAAAAAAAAAAA', True)
+
+def test_valid_creation(chan1):
+    id, name, is_public = chan1
+    new_channel = channels_create_v1(id, name, is_public)
     assert(new_channel['channel_id'] == 1)
+
+# Test for InputError when name < 1 char long
+def test_exception_too_short(invalid_chan1): 
+    id, name, is_public = invalid_chan1
+    with pytest.raises(InputError):
+        assert(channels_create_v1(id, name, is_public) == 1)
+
+# Test for InputError when name > 20 char long
+def test_exception_too_long(invalid_chan2): 
+    id, name, is_public = invalid_chan2
+    with pytest.raises(InputError):
+        assert(channels_create_v1(id, name, is_public) == 1)
