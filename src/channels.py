ce0f94c40f8f28e823fffe879451db61944d3ac0
code-monke <50727669+code-monke@users.noreply.github.com>
2021-09-22 15:56:50 +1000

    Implemented channels_create_v1

--- src/channels.py
+++ src/channels.py
@@ -1,3 +1,6 @@
+from src.data_store import data_store
+from src.error import InputError
+
 def channels_list_v1(auth_user_id):
     return {
         'channels': [
@@ -19,6 +22,20 @@ def channels_listall_v1(auth_user_id):
     }
 
 def channels_create_v1(auth_user_id, name, is_public):
-    return {
-        'channel_id': 1,
-    }
+    if len(name) < 1 or len(name) > 20:
+        raise InputError
+        return None
+
+    else:
+        store = data_store.get()
+        channels = store['channels']
+        new_channel = {
+            'id': len(channels) + 1,
+            'name' : name,
+            'is_public' : is_public,
+            'owner_members' : [auth_user_id],
+            'all_members' : [auth_user_id]
+            }
+        data_store.set(store)
+
+        return int(new_channel['id'])
