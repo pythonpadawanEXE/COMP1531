3859f1960407cd713c0af23c94bf6aaa7d5bf598
z5165158 <jake.edwards1@student.unsw.edu.au>
2021-09-23 22:29:04 +1000

    DRAFT INPUT TESTS MADE
    - Frames for input tests have been filled in, covering
    the main likely types of input errors.
    - The tests are not exhaustive and largely do not look for gaps in the specification.
    - Will Password or other input warnings be required in the future e.g. weak password?
    
    Tracks: #3

--- tests/auth_register_test.py
+++ tests/auth_register_test.py
@@ -1,84 +1,109 @@
-import auth
+from src import auth
 import re
+import pytest
 '''
 Valid Email Tests
-'^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}$'
 '''
+#r"^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}$"
+
+def test_valid_email_1():
+    result = auth.auth_register_v1('validemail@gmail.com', '123abc!@#', 'Hayden', 'Everest')
+    assert isinstance(result,int)
 
 '''
 Invalid Email Tests
-'^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}$'
 '''
+#r"^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}$"
+
+def test_invalid_email_1():
+    with pytest.raises(InputError):
+        result = auth.auth_register_v1('invalidemailgmail.com', '123abc!@#', 'Hayden', 'Everest')
 
 
-
 '''
-email address is already being used by another user
+Invalid Email address is already being used by another user
 '''
-def test_duplicate_email ():
+def test_duplicate_email():
 
     result = auth.auth_register_v1('duplicateemail@gmail.com', '123abc!@#', 'Hayden', 'Everest')
     with pytest.raises(InputError):
         auth.auth_register_v1('duplicateemail@gmail.com', '123abc!@#', 'Hayden', 'Everest') # Expect fail since we already registered
 
+'''
+Valid Multiple Registrations with unique emails
+'''
+def test_multiple_emails():
+    result = auth.auth_register_v1('distinctemail1@gmail.com', '123abc!@#', 'Hayden', 'Everest')
+    assert isinstance(result,int)
+    result = auth.auth_register_v1('distinctemail2@gmail.com', '123abc!@#', 'Hayden', 'Everest')
+    assert isinstance(result,int)
+    result = auth.auth_register_v1('distinctemail3@gmail.com', '123abc!@#', 'Hayden', 'Everest')
+    assert isinstance(result,int)
+'''
+Invalid length of password is less than 6 characters
+'''
+def test_invalid_short_password():
+    with pytest.raises(InputError):
+        result = auth.auth_register_v1('validemail@gmail.com', 'short', 'Hayden', 'Everest')
 
 '''
-length of password is less than 6 characters
+Valid length of password is more than 6 characters
 '''
-def test_short_password():
-
-
+def test_invalid_short_password():
+    result = auth.auth_register_v1('validemail@gmail.com', 'nottooshort', 'Hayden', 'Everest')
+    assert isinstance(result,int)
 
 '''
-length of name_first is not between 1 and 50 characters inclusive
+Invalid length of name_first is not between 1 and 50 characters inclusive
 '''
 
 def test_name_first_invalid_length_short():
-
+    with pytest.raises(InputError):
+        result = auth.auth_register_v1('validemail@gmail.com', '123ab78', '', 'Lastname')
 
 def test_name_first_invalid_length_long():
-
+    str_name = 'TpRXVggwnkBUGjXLVmwEGatLCEpUtbfVFLhDQUqLztGqxnrhruFyPmG'
+    with pytest.raises(InputError):
+        result = auth.auth_register_v1('validemail@gmail.com', '123ab78', str_name, 'Lastname')
 '''
-length of name_last is not between 1 and 50 characters inclusive
+Valid length of name_first is  between 1 and 50 characters inclusive
+'''
+def test_name_first_valid_length_1():
+    str_name = 'John'
+    result = auth.auth_register_v1('validemail@gmail.com', '123ab78', str_name, 'Lastname')
+    assert isinstance(result,int)
+
+def test_name_first_valid_length_2():
+    str_name = 'John-Mayer'
+    result = auth.auth_register_v1('validemail@gmail.com', '123ab78', str_name, 'Lastname')
+    assert isinstance(result,int)
+'''
+Invalid length of name_last is not between 1 and 50 characters inclusive
 '''
 def test_name_last_invalid_length_short():
-
+    with pytest.raises(InputError):
+        result = auth.auth_register_v1('validemail@gmail.com', '123ab78', 'Firstname', '')
 
 def test_name_last_invalid_length_long():
+    str_name = 'TpRXVggwnkBUGjXLVmwEGatLCEpUtbfVFLhDQUqLztGqxnrhruFyPmG'
+    with pytest.raises(InputError):
+        result = auth.auth_register_v1('validemail@gmail.com', '123ab78', 'Firstname', str_name)
+
+'''
+Valid length of name_last is  between 1 and 50 characters inclusive
+'''
+def test_name_last_valid_length_1():
+    str_name = 'John'
+    result = auth.auth_register_v1('validemail@gmail.com', '123ab78', 'Firstname', str_name)
+    assert isinstance(result,int)
+
+def test_name_last_valid_length_2():
+    str_name = 'John-Mayer'
+    result = auth.auth_register_v1('validemail@gmail.com', '123ab78', 'Firstname', str_name)
+    assert isinstance(result,int)
+
+
 
 #     PASSWORD FUTURE WARNINGS
 # =================================
-
-#Turn these into warnings for auth_register.py
-def test_horrible_password(capfd):
-    check_password("password") 
-    out, err = capfd.readouterr()
-    assert out == "Horrible password\n"
-
-def test_poor_password(capfd):
-    check_password("abba") 
-    out, err = capfd.readouterr()
-    assert out == "Poor password\n"
-
-def test_moderate_password(capfd):
-    check_password("iamgreatatthis1") 
-    out, err = capfd.readouterr()
-    assert out == "Moderate password\n"
-
-
-def test_strong_password(capfd):
-    check_password("IreallylikeCOMP1531") 
-    out, err = capfd.readouterr()
-    assert out == "Strong password\n"
-
-"""
-    Test checks if assertion error is raised when password is empty as no spec specifies that the password cannot be empty
-    but it is expected that it shouldn't be.
-    https://docs.pytest.org/en/stable/capture.html#accessing-captured-output-from-a-test-function
-    https://stackoverflow.com/questions/23337471/how-to-properly-assert-that-an-exception-gets-raised-in-pytest 
-
-"""
-def test_empty_password(capfd):
-    with pytest.raises(Exception) as excinfo:   
-        check_password("")
-    assert str(excinfo.value) == "password should not be empty" 
+#???
