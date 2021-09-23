fbc0770758f2c39a4b42a774b220a93c6188f99c
z5165158 <jake.edwards1@student.unsw.edu.au>
2021-09-24 08:59:05 +1000

    TESTS MODIFIED - test multiple emails almost complete - are more tests needed for str_handle?
    Tracking: #3

--- tests/auth_register_test.py
+++ tests/auth_register_test.py
@@ -1,6 +1,8 @@
 from src import auth
 import re
 import pytest
+from src.data_store import data_store
+from src.error import InputError
 '''
 Valid Email Tests
 '''
@@ -29,16 +31,69 @@ def test_duplicate_email():
     with pytest.raises(InputError):
         auth.auth_register_v1('duplicateemail@gmail.com', '123abc!@#', 'Hayden', 'Everest') # Expect fail since we already registered
 
+
+
 '''
 Valid Multiple Registrations with unique emails
 '''
 def test_multiple_emails():
-    result = auth.auth_register_v1('distinctemail1@gmail.com', '123abc!@#', 'Hayden', 'Everest')
-    assert isinstance(result,int)
-    result = auth.auth_register_v1('distinctemail2@gmail.com', '123abc!@#', 'Hayden', 'Everest')
-    assert isinstance(result,int)
-    result = auth.auth_register_v1('distinctemail3@gmail.com', '123abc!@#', 'Hayden', 'Everest')
-    assert isinstance(result,int)
+    result1 = auth.auth_register_v1('distinctemail1@gmail.com', '123abc!@#', 'Jake', 'Everest')
+    assert isinstance(result1,int)
+    result2 = auth.auth_register_v1('distinctemail2@gmail.com', '123abc!@#', 'Bake', 'Everest')
+    assert isinstance(result2,int)
+    result3 = auth.auth_register_v1('distinctemail3@gmail.com', '123abc!@#', 'Cake', 'Everest')
+    assert isinstance(result3,int)
+    assert result2 - result1 == 1
+    assert result3 - result2 == 1
+
+'''
+Valid Multiple Registrations with unique lowercase handles
+'''
+def test_multiple_emails():
+    result1 = auth.auth_register_v1('distinctemail1@gmail.com', '123abc!@#', 'Jake', 'Everest')
+    assert isinstance(result1,int)
+    result2 = auth.auth_register_v1('distinctemail2@gmail.com', '123abc!@#', 'Jake', 'Everest')
+    assert isinstance(result2,int)
+    result3 = auth.auth_register_v1('distinctemail3@gmail.com', '123abc!@#', 'Jake', 'Everest')
+    assert isinstance(result3,int)
+    assert result2 - result1 == 1
+    assert result3 - result2 == 1
+    #TODO: then assert datastore to check all contained handles are unique and in lower case
+
+'''
+data = {
+    'users': [
+        {
+            'u_id': 1,
+            'email' : 'jake@gmail.com',
+            'name_first' : 'Jake',
+            'name_last'  : 'Edwards',
+            'handle_str' : 'jakeedwards'
+
+        },
+        {
+            'u_id': 2,
+            'email' : 'jake2@gmail.com',
+            'name_first' : 'Jake',
+            'name_last'  : 'Edwards',
+            'handle_str' : 'jakeedwards0'
+        }
+    ],
+
+    'passwords' : [
+        {
+            'u_id':1,
+            'password' : 'Password',
+        }
+        {
+            'u_id':2,
+            'password' : 'Password',
+        }
+
+    ]
+
+'''
+
 '''
 Invalid length of password is less than 6 characters
 '''
