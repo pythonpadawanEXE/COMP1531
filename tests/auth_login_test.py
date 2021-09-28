d8ece3015ed0efd1482bd476c799fbaf9670b1d0
z5165158 <jake.edwards1@student.unsw.edu.au>
2021-09-28 21:49:18 +1000

    TESTS IMPLEMENTED FOR AUTH LOGIN - Test Input errors when email entered doesnt belong to a registered user -Test Input errors when password is not correct -Vary ordering and number of users for testing
    Tracking: #1

---
+++ tests/auth_login_test.py
@@ -0,0 +1,145 @@
+from src import auth
+import re
+import pytest
+from src.data_store import data_store
+from src.error import InputError
+from src import other
+
+#test for standard login
+def test_valid_login_1():
+    other.clear_v1()
+    result = auth.auth_register_v1('validemail@gmail.com', '123abc!@#', 'Hayden', 'Everest')
+    assert isinstance(result['auth_user_id'],int)
+    result = auth.auth_login_v1('validemail@gmail.com', '123abc!@#')
+    assert isinstance(result['auth_user_id'],int)
+
+#multiple  register and one login
+def test_valid_login_multi_registered():
+    other.clear_v1()
+    result = auth.auth_register_v1('validemail@gmail.com', '123abc!@#', 'Hayden', 'Everest')
+    assert isinstance(result['auth_user_id'],int)
+    result = auth.auth_register_v1('validemail1@gmail.com', '123abc!@#1', 'Hayden', 'Everest')
+    assert isinstance(result['auth_user_id'],int)
+    result = auth.auth_register_v1('validemail2@gmail.com', '123abc!@#2', 'Hayden', 'Everest')
+    assert isinstance(result['auth_user_id'],int)
+    result = auth.auth_register_v1('validemail3@gmail.com', '123abc!@#3', 'Hayden', 'Everest')
+    assert isinstance(result['auth_user_id'],int)
+    result = auth.auth_register_v1('validemail4@gmail.com', '123abc!@#4', 'Hayden', 'Everest')
+    assert isinstance(result['auth_user_id'],int)
+
+    result = auth.auth_login_v1('validemail@gmail.com', '123abc!@#')
+    assert isinstance(result['auth_user_id'],int)
+
+#multiple logins register and login are using the same password
+def test_valid_login_multi_registered_same_password():
+    other.clear_v1()
+    result = auth.auth_register_v1('validemail@gmail.com', '123abc!@#', 'Hayden', 'Everest')
+    assert isinstance(result['auth_user_id'],int)
+    result = auth.auth_register_v1('validemail1@gmail.com', '123abc!@#', 'Hayden', 'Everest')
+    assert isinstance(result['auth_user_id'],int)
+    result = auth.auth_register_v1('validemail2@gmail.com', '123abc!@#', 'Hayden', 'Everest')
+    assert isinstance(result['auth_user_id'],int)
+    result = auth.auth_register_v1('validemail3@gmail.com', '123abc!@#', 'Hayden', 'Everest')
+    assert isinstance(result['auth_user_id'],int)
+    result = auth.auth_register_v1('validemail4@gmail.com', '123abc!@#', 'Hayden', 'Everest')
+    assert isinstance(result['auth_user_id'],int)
+    result = auth.auth_login_v1('validemail@gmail.com', '123abc!@#')
+    assert isinstance(result['auth_user_id'],int)
+
+#multiple logins register and login are  sequential operations
+def test_multiple_logins_1():
+    other.clear_v1()
+    result = auth.auth_register_v1('validemail@gmail.com', '123abc!@#', 'Hayden', 'Everest')
+    assert isinstance(result['auth_user_id'],int)
+    result = auth.auth_login_v1('validemail@gmail.com', '123abc!@#')
+    assert isinstance(result['auth_user_id'],int)
+
+    result = auth.auth_register_v1('validemail1@gmail.com', '123abc!@#', 'Hayden', 'Everest')
+    assert isinstance(result['auth_user_id'],int)
+    result = auth.auth_login_v1('validemail1@gmail.com', '123abc!@#')
+    assert isinstance(result['auth_user_id'],int)
+
+    result = auth.auth_register_v1('validemail2@gmail.com', '123abc!@#', 'Hayden', 'Everest')
+    assert isinstance(result['auth_user_id'],int)
+    result = auth.auth_login_v1('validemail2@gmail.com', '123abc!@#')
+    assert isinstance(result['auth_user_id'],int)
+
+    result = auth.auth_register_v1('validemail3@gmail.com', '123abc!@#', 'Hayden', 'Everest')
+    assert isinstance(result['auth_user_id'],int)
+    result = auth.auth_login_v1('validemail3@gmail.com', '123abc!@#')
+    assert isinstance(result['auth_user_id'],int)
+
+    result = auth.auth_register_v1('validemail4@gmail.com', '123abc!@#', 'Hayden', 'Everest')
+    assert isinstance(result['auth_user_id'],int)
+    result = auth.auth_login_v1('validemail4@gmail.com', '123abc!@#')
+    assert isinstance(result['auth_user_id'],int)
+
+#multiple logins register and login are not sequential operations
+def test_multiple_logins_2():
+    result = auth.auth_register_v1('validemail4@gmail.com', '123abc!@#', 'Hayden', 'Everest')
+    assert isinstance(result['auth_user_id'],int)
+
+    result = auth.auth_register_v1('validemail2@gmail.com', '123abc!@#', 'Hayden', 'Everest')
+    assert isinstance(result['auth_user_id'],int)
+
+    result = auth.auth_register_v1('validemail1@gmail.com', '123abc!@#', 'Hayden', 'Everest')
+    assert isinstance(result['auth_user_id'],int)
+
+    result = auth.auth_login_v1('validemail4@gmail.com', '123abc!@#')
+    assert isinstance(result['auth_user_id'],int)
+
+    result = auth.auth_login_v1('validemail2@gmail.com', '123abc!@#')
+    assert isinstance(result['auth_user_id'],int)
+
+    result = auth.auth_login_v1('validemail1@gmail.com', '123abc!@#')
+    assert isinstance(result['auth_user_id'],int)
+
+#email login doesnt match a registered user
+def test_invalid_login_email():
+    other.clear_v1()
+    result = auth.auth_register_v1('validemail@gmail.com', '123abc!@#', 'Hayden', 'Everest')
+    assert isinstance(result['auth_user_id'],int)
+    with pytest.raises(InputError):
+        result = auth.auth_login_v1('validemailgmail.com', '123abc!@#')
+
+
+
+#login password doesnt match registered password
+def test_invalid_login_password():
+    other.clear_v1()
+    result = auth.auth_register_v1('validemail@gmail.com', '123abc!@#', 'Hayden', 'Everest')
+    assert isinstance(result['auth_user_id'],int)
+    with pytest.raises(InputError):
+        result = auth.auth_login_v1('validemail@gmail.com', '123abc!@#?')
+
+#login password doesnt match registered password with multiple registrations
+def test_invalid_login_password_multi_registered():
+    other.clear_v1()
+    result = auth.auth_register_v1('validemail@gmail.com', '123abc!@#', 'Hayden', 'Everest')
+    assert isinstance(result['auth_user_id'],int)
+    result = auth.auth_register_v1('validemail1@gmail.com', '123abc!@#1', 'Hayden', 'Everest')
+    assert isinstance(result['auth_user_id'],int)
+    result = auth.auth_register_v1('validemail2@gmail.com', '123abc!@#2', 'Hayden', 'Everest')
+    assert isinstance(result['auth_user_id'],int)
+    result = auth.auth_register_v1('validemail3@gmail.com', '123abc!@#3', 'Hayden', 'Everest')
+    assert isinstance(result['auth_user_id'],int)
+    result = auth.auth_register_v1('validemail4@gmail.com', '123abc!@#4', 'Hayden', 'Everest')
+    assert isinstance(result['auth_user_id'],int)
+    with pytest.raises(InputError):
+        result = auth.auth_login_v1('validemail@gmail.com', '123abc!@#?')
+
+#login password doesnt match registered password with multiple registrations with same password
+def test_invalid_login_password_multi_registered_same_password():
+    other.clear_v1()
+    result = auth.auth_register_v1('validemail@gmail.com', '123abc!@#', 'Hayden', 'Everest')
+    assert isinstance(result['auth_user_id'],int)
+    result = auth.auth_register_v1('validemail1@gmail.com', '123abc!@#', 'Hayden', 'Everest')
+    assert isinstance(result['auth_user_id'],int)
+    result = auth.auth_register_v1('validemail2@gmail.com', '123abc!@#', 'Hayden', 'Everest')
+    assert isinstance(result['auth_user_id'],int)
+    result = auth.auth_register_v1('validemail3@gmail.com', '123abc!@#', 'Hayden', 'Everest')
+    assert isinstance(result['auth_user_id'],int)
+    result = auth.auth_register_v1('validemail4@gmail.com', '123abc!@#', 'Hayden', 'Everest')
+    assert isinstance(result['auth_user_id'],int)
+    with pytest.raises(InputError):
+        result = auth.auth_login_v1('validemail@gmail.com', '123abc!@#?')
