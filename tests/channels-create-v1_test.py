ed90e6d6558d3ec662296e14cb1a961ba0f63cca
z5360175 <caleb.watts@student.unsw.edu.au>
2021-09-28 19:32:31 +1000

    Added test to check channels_create_v1() raises an AcessError when called by an unauthorised user

--- tests/channels-create-v1_test.py
+++ tests/channels-create-v1_test.py
@@ -4,19 +4,23 @@
 import pytest
 
 from src.channels import channels_create_v1, channels_list_v1
-from src.error import InputError
+from src.auth import auth_register_v1
+from src.error import InputError, AccessError
 
 @pytest.fixture
 def chan1():
-    return (1, 'My Channel', True)
+    auth_user_id = auth_register_v1("js@email.com", "ABCDEFGH", "John", "Smith")['auth_user_id']
+    return (auth_user_id, 'My Channel', True)
 
 @pytest.fixture
 def invalid_chan1():
-    return (1, '', True)
+    auth_user_id = auth_register_v1("js@email.com", "ABCDEFGH", "John", "Smith")['auth_user_id']
+    return (auth_user_id, '', True)
 
 @pytest.fixture
 def invalid_chan2():
-    return (1, 'AAAAAAAAAAAAAAAAAAAAA', True)
+    auth_user_id = auth_register_v1("js@email.com", "ABCDEFGH", "John", "Smith")['auth_user_id']
+    return (auth_user_id, 'AAAAAAAAAAAAAAAAAAAAA', True)
 
 def test_valid_creation(chan1):
     id, name, is_public = chan1
@@ -35,3 +39,8 @@ def test_exception_too_long(invalid_chan2):
     id, name, is_public = invalid_chan2
     with pytest.raises(InputError):
         assert(channels_create_v1(id, name, is_public) == 1)
+
+def test_raise_AccessError(invalid_chan1): 
+    id, name, is_public = invalid_chan1
+    with pytest.raises(AccessError):
+        assert(channels_create_v1(id, name, is_public) == {})
