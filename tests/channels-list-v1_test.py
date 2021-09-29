a29721c8fe6d29ef8e18203b1025211b5f17355c
z5360175 <caleb.watts@student.unsw.edu.au>
2021-09-29 14:39:09 +1000

    Modified tests to align with spec expected return types

--- tests/channels-list-v1_test.py
+++ tests/channels-list-v1_test.py
@@ -11,16 +11,20 @@ def test_valid_list():
     auth_user_id_1 = auth_register_v1("js@email.com", "ABCDEFGH", "John", "Smith")['auth_user_id']
     auth_user_id_2 = auth_register_v1("jems@email.com", "ABCDEFGH", "Jemma", "Smith")['auth_user_id']
     list_of_channels = []
-    list_of_channels.append(channels_create_v1(auth_user_id_1, "Chan 1", True))
-    list_of_channels.append(channels_create_v1(auth_user_id_1, "Chan 2", True))
-    list_of_channels.append(channels_create_v1(auth_user_id_1, "Chan 3", True))
-    bad_chan = channels_create_v1(auth_user_id_2, "Chan 4", True)
+    list_of_channels.append({'channel_id' : channels_create_v1(auth_user_id_1, "Chan 1", True)['channel_id'], 'name' : "Chan 1"})
+    list_of_channels.append({'channel_id' : channels_create_v1(auth_user_id_1, "Chan 2", True)['channel_id'], 'name' : "Chan 2"})
+    list_of_channels.append({'channel_id' : channels_create_v1(auth_user_id_1, "Chan 3", True)['channel_id'], 'name' : "Chan 3"})
+    channels_dict = {'channels' : list_of_channels}
+
+    bad_chan = {'channel_id' : channels_create_v1(auth_user_id_2, "Chan 4", True)['channel_id'], 'name' : "Chan 4"}
+
     channels = channels_list_v1(auth_user_id_1)
-    for channel in list_of_channels:
-        assert(channel in channels)
-    assert(bad_chan not in channels)
+
+    for channel in channels_dict['channels']:
+        assert(channel in channels['channels'])
+    assert(bad_chan not in channels_dict['channels'])
 
 def test_raise_exception(): 
     with pytest.raises(AccessError):
-        assert(channels_list_v1(687543) == [])
+        assert(channels_list_v1(687543) == {})
 
