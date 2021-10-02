6f7c4bab6b05bbb500374279548c0c630c58049a
z5360175 <caleb.watts@student.unsw.edu.au>
2021-10-02 15:35:25 +1000

    Fixed pytest for channel_join_v1 (email duplicates)

--- tests/channel_join_v1_test.py
+++ tests/channel_join_v1_test.py
@@ -10,27 +10,27 @@ from src.error import AccessError, InputError
 
 def test_invalid_channel_id():
     clear_v1()
-    user = auth_register_v1("j@email.com", "ABCDEF123", "Cal", "Watts")
+    user = auth_register_v1("a@email.com", "ABCDEF123", "Cal", "Watts")
     with pytest.raises(InputError):
         assert(channel_join_v1(user["auth_user_id"], 999) == {})
 
 def test_already_joined():
     clear_v1()
-    user = auth_register_v1("j@email.com", "ABCDEF123", "Cal", "Watts")
+    user = auth_register_v1("b@email.com", "ABCDEF123", "Cal", "Watts")
     channel = channels_create_v1(user["auth_user_id"], "Channel 1", True)
     with pytest.raises(InputError):
         assert(channel_join_v1(user["auth_user_id"], channel["channel_id"]) == {})
 
 def test_join_private_channel():
-    owner = auth_register_v1("j@email.com", "ABCDEF123", "Cal", "Watts")
-    user = auth_register_v1("k@email.com", "ABCDEF123", "Kim", "Smith")
+    owner = auth_register_v1("c@email.com", "ABCDEF123", "Cal", "Watts")
+    user = auth_register_v1("d@email.com", "ABCDEF123", "Kim", "Smith")
     channel = channels_create_v1(owner["auth_user_id"], "Channel 1", False)
     with pytest.raises(AccessError):
         assert(channel_join_v1(user["auth_user_id"], channel["channel_id"]) == {})
 
 def test_join_public_channel():
-    owner = auth_register_v1("j@email.com", "ABCDEF123", "Cal", "Watts")
-    user = auth_register_v1("k@email.com", "ABCDEF123", "Kim", "Smith")
+    owner = auth_register_v1("e@email.com", "ABCDEF123", "Cal", "Watts")
+    user = auth_register_v1("f@email.com", "ABCDEF123", "Kim", "Smith")
     channel = channels_create_v1(owner["auth_user_id"], "Channel 1", True)
     assert(channel_join_v1(user["auth_user_id"], channel["channel_id"]) == {})
     assert(user["auth_user_id"] in channel_details_v1(owner["auth_user_id"], channel["channel_id"])["all_members"])
