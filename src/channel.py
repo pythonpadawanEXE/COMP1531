18742c7e14fe998b8b2e45ae278b4805c7d6bf4d
Mahi <starexify@gmail.com>
2021-10-02 17:22:17 +1000

    Finished name, is_public, owner_members in details dictionary * Added helper functins to retreive specific data * all_members remaining to be done
    Tracking: #10

--- src/channel.py
+++ src/channel.py
@@ -30,6 +30,44 @@ def is_user_authorised(auth_user_id, channel_id):
 
     return is_authorised
 
+# Returns the name of the channel with channel_id
+def get_channel_name(channel_id):
+    store = data_store.get()
+    channel_store = store['channels']
+
+    for chan in channel_store:
+        if chan['id'] == channel_id:
+            return chan['name']
+
+# Checks if channel with channel_id is public or private
+def is_channel_public(channel_id):
+    store = data_store.get()
+    channel_store = store['channels']
+
+    for chan in channel_store:
+        if chan['id'] == channel_id:
+            return chan['is_public']
+
+# Returns the auth_user_id of the channel owner with ID channel_id
+def get_channel_owner(channel_id):
+    store = data_store.get()
+    channels = store['channels']
+
+    for chan in channel_store:
+        if chan['id'] == channel_id:
+            return chan['owner_members'][0]
+
+# Reurns a list containing details of the owner members
+def user_details(auth_user_id):
+    user_details_list = []
+    store = data_store.get()
+    user_store = store['users']
+
+    for user in user_store:
+        if user['u_id'] == auth_user_id:
+            user_details_list.append(user)
+    return user_details_list
+
 # Given a channel with ID channel_id that the authorised user is a member of,
 # provides basic details about the channel.
 def channel_details_v1(auth_user_id, channel_id):
@@ -42,19 +80,13 @@ def channel_details_v1(auth_user_id, channel_id):
     if not is_user_authorised(auth_user_id, channel_id):
         raise AccessError
 
+    channel_owner_id = get_channel_owner(channel_id)
+
     # Return channel details
     return {
-        'name': 'Hayden',
-        'is_public': True,
-        'owner_members': [
-            {
-                'u_id': 1,
-                'email': 'example@gmail.com',
-                'name_first': 'Hayden',
-                'name_last': 'Jacobs',
-                'handle_str': 'haydenjacobs',
-            }
-        ],
+        'name': get_channel_name(channel_id),
+        'is_public': is_channel_public(channel_id),
+        'owner_members': user_details(channel_owner_id),
         'all_members': [
             {
                 'u_id': 1,
