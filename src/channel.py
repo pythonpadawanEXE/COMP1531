bec1455164f4e25b0ff3b68b4b9d49033f3ae1b9
Mahi <starexify@gmail.com>
2021-10-02 16:44:55 +1000

    channel_details_v1 errors * Added Input and Access errors * Added helper functions to check channel validity and user exists in a   channel

--- src/channel.py
+++ src/channel.py
@@ -1,17 +1,48 @@
-from src.channels import verify_user_id
+from src.data_store import data_store
+from src.error import InputError, AccessError
 
 def channel_invite_v1(auth_user_id, channel_id, u_id):
     return {
     }
 
-def is_channel_valid():
-    pass
+# Check if the channel with channel_id is valid
+def is_channel_valid(channel_id):
+
+    channel_valid = False
+    store = data_store.get()
+    channel_store = store['channels']
+
+    for chan in channel_store:
+        if channel_id in chan['id']:
+            channel_valid = True
+
+    return channel_valid
+
+# Checks if user with auth_user_id is in channel with channel_id
+def is_user_authorised(auth_user_id, channel_id):
+    is_authorised = False
+    store = data_store.get()
+    channel_store = store['channels']
+
+    for chan in channel_store:
+        if auth_user_id in chan['all_members']:
+            is_authorised = True
+
+    return is_authorised
 
 # Given a channel with ID channel_id that the authorised user is a member of,
 # provides basic details about the channel.
 def channel_details_v1(auth_user_id, channel_id):
 
-    # Return type format example
+    # channel_id does not refer to a valid channel
+    if not is_channel_valid(channel_id):
+        raise InputError
+
+    # channel_id is valid and the authorised user is not a member of the channel
+    if not is_user_authorised(auth_user_id, channel_id):
+        raise AccessError
+
+    # Return channel details
     return {
         'name': 'Hayden',
         'is_public': True,
