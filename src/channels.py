7361b021d15b47f0d1182eed712c88be0a64e947
z5360175 <caleb.watts@student.unsw.edu.au>
2021-10-03 16:14:27 +1100

    Fixed style

--- src/channels.py
+++ src/channels.py
@@ -1,17 +1,27 @@
+""" This module handles the creation and listing of channels.
+
+    Functions:
+        verify_user_id(auth_user_id) -> boolean
+        channels_list_v1(auth_user_id) -> { channels }
+        channels_listall_v1(auth_user_id) -> { channels }
+        channels_create_v1(auth_user_id, name, is_public) -> { channel_id }
+
+"""
+
 from src.data_store import data_store
 from src.error import InputError, AccessError
 
-''' Helper function that verifies that the user exists in the data store, if in data store returns true else false.
-
-    Arguments:
-        auth_user_id (int)    - The user id of the user being verified.
-
-    Return Value:
-        Returns True on user id being found.
-        Returns False on user id not being found.
-
-'''
 def verify_user_id(auth_user_id):
+    """ Helper function that verifies that the user exists in the data store,
+        if in data store returns true else false.
+
+        Arguments:
+            auth_user_id (int)    - The user id of the user being verified.
+
+        Return Value:
+            Returns True on user id being found.
+            Returns False on user id not being found.
+    """
     is_authorised = False
     store = data_store.get()
 
@@ -21,24 +31,26 @@ def verify_user_id(auth_user_id):
             is_authorised = True
     return is_authorised
 
-''' Lists all channels that the given user id is a member of.
-
-    Arguments:
-        auth_user_id (int)    - The user id of the user whose channel membership is being listed.
-
-    Exceptions:
-        AccessError - Occurs when the user's id does not exist in the data store.
-
-    Return Value:
-        Returns { channels } on successful completion.
-'''
 def channels_list_v1(auth_user_id):
+    """ Lists all channels that the given user id is a member of.
+
+        Arguments:
+            auth_user_id (int)    - The user id of the user whose channel membership
+                                    is being listed.
+
+        Exceptions:
+            AccessError - Occurs when the user's id does not exist in the data store.
+
+        Return Value:
+            Returns { channels } on successful completion.
+    """
 
     # Verifies that the user exists in the data store, raises an AccessError otherwise.
-    if verify_user_id(auth_user_id) != True:
+    if not verify_user_id(auth_user_id):
         raise AccessError
 
-    # Iterates through the list of channels and returns the subset that the given user is a member of.
+    # Iterates through the list of channels and
+    # returns the subset that the given user is a member of.
     store = data_store.get()
     channel_store = store['channels']
     channels = []
@@ -49,18 +61,18 @@ def channels_list_v1(auth_user_id):
         'channels' : channels
     }
 
-''' Lists all channels that exist including public and private channels.
-
-    Arguments:
-        auth_user_id (int)    - The user id of the calling user.
-
-    Exceptions:
-        AccessError - Occurs when the user's id does not exist in the data store.
-
-    Return Value:
-        Returns { channels } on successful completion.
-'''
 def channels_listall_v1(auth_user_id):
+    """ Lists all channels that exist including public and private channels.
+
+        Arguments:
+            auth_user_id (int)    - The user id of the calling user.
+
+        Exceptions:
+            AccessError - Occurs when the user's id does not exist in the data store.
+
+        Return Value:
+            Returns { channels } on successful completion.
+    """
 
     # Verifies that the user exists in the data store, raises an AccessError otherwise.
     if not verify_user_id(auth_user_id):
@@ -79,26 +91,25 @@ def channels_listall_v1(auth_user_id):
         'channels' : channels
     }
 
-''' Creates a channel as specified by the parameters.
-
-Arguments:
-    auth_user_id (int)    - The user id of the calling user.
-    name (string)         - The name of the channel to be created.
-    is_public (boolean)   - Determines whether the channel is public
-    ...
-
-Exceptions:
-    InputError  - Occurs when length of name is less than 1 or more than 20 characters.
-    AccessError - Occurs when the user's id does not exist in the data store.
-
-Return Value:
-    Returns { channel_id } on successful completion.
-
-'''
 def channels_create_v1(auth_user_id, name, is_public):
+    """ Creates a channel as specified by the parameters.
+
+    Arguments:
+        auth_user_id (int)    - The user id of the calling user.
+        name (string)         - The name of the channel to be created.
+        is_public (boolean)   - Determines whether the channel is public
+        ...
+
+    Exceptions:
+        InputError  - Occurs when length of name is less than 1 or more than 20 characters.
+        AccessError - Occurs when the user's id does not exist in the data store.
+
+    Return Value:
+        Returns { channel_id } on successful completion.
+    """
 
     # Verifies that the user exists in the data store, raises an AccessError otherwise.
-    if verify_user_id(auth_user_id) != True:
+    if not verify_user_id(auth_user_id):
         raise AccessError
 
     # Verifies that the channel name is of correct length, raises an InputError otherwise.
@@ -112,22 +123,20 @@ def channels_create_v1(auth_user_id, name, is_public):
     #     'is_public'     :   boolean
     #     'owner_members' :   list of user ids - creator made an owner
     #     'all_members'   :   list of user ids - creator made a member
-    #     'messages'      :   list of dictionaries for message details i.e.  { message_id, u_id, message, time_created }
-    else:
-        store = data_store.get()
-        channels = store['channels']
-        new_channel = {
-            'id': len(channels) + 1,
-            'name' : name,
-            'is_public' : is_public,
-            'owner_members' : [auth_user_id],
-            'all_members' : [auth_user_id],
-            'messages' :[],
-            }
-        channels.append(new_channel)
-        data_store.set(store)
-
-        return {
-            'channel_id' : new_channel['id']
+    #     'messages'      :   list of dictionaries for message details i.e.
+    #                         { message_id, u_id, message, time_created }
+    store = data_store.get()
+    channels = store['channels']
+    new_channel = {
+        'id': len(channels) + 1,
+        'name' : name,
+        'is_public' : is_public,
+        'owner_members' : [auth_user_id],
+        'all_members' : [auth_user_id],
+        'messages' :[],
         }
-            
+    channels.append(new_channel)
+    data_store.set(store)
+    return {
+        'channel_id' : new_channel['id']
+    }
