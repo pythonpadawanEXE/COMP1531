622c1558a210b844f93d8d635a8de21001b57d80
Mahi <starexify@gmail.com>
2021-10-20 00:09:13 +1100

    Added new black box tests for channel_details_v2: * Tracking issue: #23

--- tests/channel_details_v2_test.py
+++ tests/channel_details_v2_test.py
@@ -2,14 +2,8 @@
 # pytest file to test the implementation of channel_details_v2 endpoint
 
 import pytest
-
-from src.error import AccessError
-from src.error import InputError
-
-from src.auth import auth_register_v1
-from src.channel import channel_invite_v1, channel_details_v1, channel_join_v1
-from src.channels import channels_create_v1
-from src.other import clear_v1
+import requests
+from src import config
 
 @pytest.fixture(autouse=True)
 def clear():
@@ -25,10 +19,10 @@ def clear():
 def register_user(email, password, name_first, name_last):
 
     '''
-    Registers a new user with given parameters and returns the users token
+    Registers a new user with given parameters and returns the users uid and token
     '''
 
-    response = requests.post(f"{BASE_URL}/auth/register/v2",json={
+    response = requests.post(f"{BASE_URL}auth/register/v2",json={
         'email' : email,
         'password' : password,
         'name_first' : name_first,
@@ -38,17 +32,46 @@ def register_user(email, password, name_first, name_last):
     assert response.status_code == 200
     response_data = response.json()
     assert isinstance(response_data['token'],str)
-    assert isinstance(response_data['user_token'],int)
-    return response_data['token']
+    assert isinstance(response_data['auth_user_id'],int)
+    return response_data
+
+def channels_create(token, name, is_public):
+
+    '''
+    Creates a channel for user with given token and returns the channel ID
+    '''
+
+    response = requests.post(f"{BASE_URL}channels/create/v2", json={
+        'token' : token,
+        'name' : name,
+        'is_public' : is_public
+    })
+
+    assert response.status_code == 200
+    response_data = response.json()
+    return response_data
+
+def channel_details(token, channel_id):
+
+    '''
+    Creates a channel for user with given token and returns the channel ID
+    '''
+
+    response = requests.get(f"{BASE_URL}channel/details/v2?token={token}&channel_id={channel_id}")
+    assert response.status_code == 200
+    response_data = response.json()
+    return response_data
 
 # Test for a single member with a public channel
 def test_details_public_individual():
     # New user
-    auth_user_id = auth_register_v1("js@email.com", "ABCDEFGH", "John", "Smith")['auth_user_id']
+    user = register_user("js@email.com", "ABCDEFGH", "John", "Smith")
+    token = user['token']
+    auth_user_id = user['auth_user_id']
     # Channel ID
-    channel_id = channels_create_v1(auth_user_id, "Chan 1", True)['channel_id']
+    channel_id = channels_create(token, "Chan 1", True)['channel_id']
     # Channel details
-    channel_details = channel_details_v1(auth_user_id, channel_id)
+    channel_details = channel_details(token, channel_id)
 
     # Loop through the channel details and find if auth_user_id is in all_members
     for user in channel_details['all_members']:
@@ -57,11 +80,13 @@ def test_details_public_individual():
 # Test for a single member with a private channel
 def test_details_private_individual():
     # New user
-    auth_user_id = auth_register_v1("js@email.com", "ABCDEFGH", "John", "Smith")['auth_user_id']
+    user = register_user("js@email.com", "ABCDEFGH", "John", "Smith")
+    token = user['token']
+    auth_user_id = user['auth_user_id']
     # Channel ID
-    channel_id = channels_create_v1(auth_user_id, "Chan 1", False)['channel_id']
+    channel_id = channels_create(token, "Chan 1", False)['channel_id']
     # Channel details
-    channel_details = channel_details_v1(auth_user_id, channel_id)
+    channel_details = channel_details(token, channel_id)
 
     # Loop through the channel details and find if auth_user_id is in all_members
     for user in channel_details['all_members']:
@@ -70,13 +95,15 @@ def test_details_private_individual():
 # Test for a single member with public and private channels
 def test_details_mixed_individual():
     # New user
-    auth_user_id = auth_register_v1("js@email.com", "ABCDEFGH", "John", "Smith")['auth_user_id']
+    user = register_user("js@email.com", "ABCDEFGH", "John", "Smith")
+    token = user['token']
+    auth_user_id = user['auth_user_id']
     # Channel ID
-    channel_id = channels_create_v1(auth_user_id, "Chan 1", False)['channel_id']
+    channel_id = channels_create(token, "Chan 1", False)['channel_id']
     # Create a public channel
-    channels_create_v1(auth_user_id, "Chan 2", True)['channel_id']
+    channels_create(token, "Chan 2", True)['channel_id']
     # Channel details
-    channel_details = channel_details_v1(auth_user_id, channel_id)
+    channel_details = channel_details(token, channel_id)
 
     # Loop through the channel details and find if auth_user_id is in all_members
     for user in channel_details['all_members']:
@@ -85,16 +112,21 @@ def test_details_mixed_individual():
 # Test for multiple members with a public channel
 def test_details_public_multiple():
     # New user
-    auth_user_id_1 = auth_register_v1("js@email.com", "ABCDEFGH", "John", "Smith")['auth_user_id']
+    user_1 = register_user("js@email.com", "ABCDEFGH", "John", "Smith")
+    token_1 = user_1['token']
+    auth_user_id_1 = user_1['auth_user_id']
+
     # New user 2
-    auth_user_id_2 = auth_register_v1("jems@email.com", "ABCDEFGH", "Jemma", "Smith")['auth_user_id']
+    user_2 = register_user("jems@email.com", "ABCDEFGH", "Jemma", "Smith")
+    token_2 = user_2['token']
+    auth_user_id_2 = user_2['auth_user_id']
 
     # Channel ID
-    channel_id = channels_create_v1(auth_user_id_1, "Chan 1", True)['channel_id']
+    channel_id = channels_create(token_1, "Chan 1", True)['channel_id']
     # Invite auth_user_id_2 to channel
-    channel_join_v1(auth_user_id_2, channel_id)
+    channel_join(token_2, channel_id)
     # Channel details
-    channel_details = channel_details_v1(auth_user_id_1, channel_id)
+    channel_details = channel_details(token_1, channel_id)
 
     # Loop through the channel details and find if auth_user_id and auth_user_id2 is in the channel
     for user in channel_details['all_members']:
@@ -103,18 +135,22 @@ def test_details_public_multiple():
 # Test for invalid channel ID
 def test_invalid_channel():
     # New user
-    auth_user_id_1 = auth_register_v1("js@email.com", "ABCDEFGH", "John", "Smith")['auth_user_id']
-    with pytest.raises(InputError):
-        assert(channel_details_v1(auth_user_id_1, 9999))
+    user = register_user("js@email.com", "ABCDEFGH", "John", "Smith")
+    token = user['token']
+
+    response = requests.get(f"{BASE_URL}channel/details/v2?token={token}&channel_id={9999}")
+    assert response.status_code == 400
 
 # Test for valid channel ID and the authorised user is not a member of the channel
 def test_user_unauthorised():
     # New user
-    auth_user_id_1 = auth_register_v1("js@email.com", "ABCDEFGH", "John", "Smith")['auth_user_id']
+    user_1 = register_user("js@email.com", "ABCDEFGH", "John", "Smith")
+    token_1 = user_1['token']
     # New user 2
-    auth_user_id_2 = auth_register_v1("jems@email.com", "ABCDEFGH", "Jemma", "Smith")['auth_user_id']
+    user_2 = register_user("jems@email.com", "ABCDEFGH", "Jemma", "Smith")
+    token_2 = user_2['token']
     # Channel ID
-    channel_id = channels_create_v1(auth_user_id_1, "Chan 1", True)['channel_id']
+    channel_id = channels_create(token_1, "Chan 1", True)['channel_id']
 
-    with pytest.raises(AccessError):
-        assert(channel_details_v1(auth_user_id_2, channel_id))
+    response = requests.get(f"{BASE_URL}channel/details/v2?token={token_2}&channel_id={channel_id}")
+    assert response.status_code == 403
