0c8a845199528ddb7cdf14ab8d3325d25ba09d5a
z5360175 <caleb.watts@student.unsw.edu.au>
2021-10-20 03:00:01 +1100

    Implemented tests in channel_join_v2_test.py

--- tests/channel_join_v2_test.py
+++ tests/channel_join_v2_test.py
@@ -8,9 +8,10 @@ from src import config
 @pytest.fixture
 def setup():
     '''
-    Clears the server data and creates some users
+    Clears the server data and creates some users and channels.
     '''
     user = []
+    channels = []
     requests.delete(config.url + 'clear/v1')
     resp = requests.post(config.url + f"auth/register/v2", json={
         'email' : 'validemail@gmail.com',
@@ -26,23 +27,45 @@ def setup():
         'name_last' : 'Smith'
     })
     user.append(json.loads(resp.text))
-    return user
+    resp = requests.post(config.url + f"auth/register/v2", json={
+        'email' : 'validemail2@gmail.com',
+        'password' : '123abc!@#',
+        'name_first' : 'Jade',
+        'name_last' : 'Painter'
+    })
+    user.append(json.loads(resp.text))
+    channels.append(requests.post(config.url + 'channels/create/v2', json={'token': user[0]['token'], 'name': 'My channel', 'is_public': 'True'}))
+    channels.append(requests.post(config.url + 'channels/create/v2', json={'token': user[1]['token'], 'name': 'My private channel', 'is_public': 'False'}))
+    return (user, channels)
 
-
-def test_invalid_channel_id():
-    pass
+def test_invalid_channel_id(setup):
+    users, _ = setup
+    resp = requests.post(config.url + 'channel/join/v2', json={'token': users[0]['token'], 'channel_id': 1234})
+    assert resp.status_code == 400
         
-def test_invalid_auth_user_id():
-    pass
+def test_invalid_token(setup):
+    _, channels = setup
+    resp = requests.post(config.url + 'channel/join/v2', json={'token': "", 'channel_id': channels[0]["channel_id"]})
+    assert resp.status_code == 403
         
-def test_already_joined():
-    pass
+def test_already_joined(setup):
+    users, channels = setup
+    resp = requests.post(config.url + 'channel/join/v2', json={'token': users[1]['token'], 'channel_id': channels[0]["channel_id"]})
+    assert resp.status_code == 403
 
-def test_join_private_channel():
-    pass
+def test_join_private_channel(setup):
+    users, channels = setup
+    resp = requests.post(config.url + 'channel/join/v2', json={'token': users[2]['token'], 'channel_id': channels[1]["channel_id"]})
+    assert resp.status_code == 400
 
-def test_join_public_channel():
-    pass
+def test_join_public_channel(setup):
+    users, channels = setup
+    _ = requests.post(config.url + 'channel/join/v2', json={'token': users[1]['token'], 'channel_id': channels[0]["channel_id"]})
+    channel_details = requests.get(config.url + 'channel/details/v2', params={'token': users[1]['token'], 'channel_id': channels[0]["channel_id"]})
+    assert channel_details.status_code == 200
 
-def test_global_owner_join_private_channel():
-    pass
+def test_global_owner_join_private_channel(setup):
+    users, channels = setup
+    _ = requests.post(config.url + 'channel/join/v2', json={'token': users[0]['token'], 'channel_id': channels[1]["channel_id"]})
+    channel_details = requests.get(config.url + 'channel/details/v2', params={'token': users[0]['token'], 'channel_id': channels[1]["channel_id"]})
+    assert channel_details.status_code == 200
