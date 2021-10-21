80660a841510ccfc652873588707242fb26e55b8
z5360175 <caleb.watts@student.unsw.edu.au>
2021-10-21 14:26:29 +1100

    Added test_user_not_global_owner to pytests in admin_user_remove_v1.py

--- tests/admin_user_remove_v1.py
+++ tests/admin_user_remove_v1.py
@@ -5,8 +5,40 @@ import requests
 import json
 from src import config
 
-def test_user_not_global_owner():
-    pass
+@pytest.fixture
+def setup():
+    '''
+    Clears the server data and creates some users.
+    '''
+    users = []
+    requests.delete(config.url + 'clear/v1')
+    resp = requests.post(config.url + f"auth/register/v2", json={
+        'email' : 'validemail@gmail.com',
+        'password' : '123abc!@#',
+        'name_first' : 'Hayden',
+        'name_last' : 'Everest'
+    })
+    users.append(json.loads(resp.text))
+    resp = requests.post(config.url + f"auth/register/v2", json={
+        'email' : 'validemail2@gmail.com',
+        'password' : '123abc!@#',
+        'name_first' : 'Robert',
+        'name_last' : 'Reid'
+    })
+    users.append(json.loads(resp.text))
+    resp = requests.post(config.url + f"auth/register/v2", json={
+        'email' : 'validemail3@gmail.com',
+        'password' : '123abc!@#',
+        'name_first' : 'Jade',
+        'name_last' : 'Painter'
+    })
+    users.append(json.loads(resp.text))
+    return users
+
+def test_user_not_global_owner(setup):
+    users = setup
+    resp = requests.delete(config.url + 'admin/user/remove/v1', params={'token': users[1]['token'], 'u_id': users[2]['auth_user_id']})
+    assert resp.status_code == 403
 
 def test_u_id_not_valid():
     pass
