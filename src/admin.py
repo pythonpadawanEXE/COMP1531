bc9aee67b03eb60e6ba1388ed64b6bb273529787
z5360175 <caleb.watts@student.unsw.edu.au>
2021-10-21 15:17:41 +1100

    Added admin/user/remove/v1 implementation in admin.py and users.py

--- src/admin.py
+++ src/admin.py
@@ -1,5 +1,49 @@
-def admin_user_remove_v1():
-    pass
+from src.data_store import data_store
+from src.error import InputError, AccessError
+from src.other import verify_user_id
+
+def admin_user_remove_v1(auth_user_id, u_id):
+    if not verify_user_id(auth_user_id):
+        raise AccessError(description="User ID does not exist.")
+    
+    found = False
+    target_user = {}
+    store = data_store.get()
+    users_store = store['users']
+    for user in users_store:
+        if u_id == user['u_id']:
+            found = True
+            target_user = user
+
+    if not found:
+        raise InputError(description="u_id does not refer to a valid user")
+
+    # Purge user details
+    target_user['name_first'] = 'Removed'
+    target_user['name_first'] = 'user'
+    target_user['email'] = ""
+    target_user['handle_str'] = "Removed user"
+    
+    # Purge user from channel membership
+    channels_store = store['channels']
+    for channel in channels_store:
+        channel['owner_members'].remove(u_id)
+        channel['all_members'].remove(u_id)
+
+    # Purge user from dms
+    # dms_store = store['dms']
+    # for dm in dms_store:
+    #     if dm['u_id'] == u_id or dm['owner'] == u_id:
+    #         dm['message'] = "Removed user"
+    #     dm_leave_v1(dm)
+
+    # Purge user's message contents
+    messages_store = store['messages']
+    for message in messages_store:
+        if message['u_id'] == u_id:
+            message['message'] = "Removed user"
+
+    return {}
 
 def admin_userpermission_change_v1():
     pass
