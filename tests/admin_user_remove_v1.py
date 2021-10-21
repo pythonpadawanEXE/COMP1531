ada891dc9ffcfca698b78aa86925aebb787bdf78
z5360175 <caleb.watts@student.unsw.edu.au>
2021-10-21 14:51:13 +1100

    Added tests to admin_user_remove_v1_test.py

--- tests/admin_user_remove_v1.py
+++ tests/admin_user_remove_v1.py
@@ -40,11 +40,17 @@ def test_user_not_global_owner(setup):
     resp = requests.delete(config.url + 'admin/user/remove/v1', params={'token': users[1]['token'], 'u_id': users[2]['auth_user_id']})
     assert resp.status_code == 403
 
-def test_u_id_not_valid():
-    pass
+def test_u_id_not_valid(setup):
+    users = setup
+    resp = requests.delete(config.url + 'admin/user/remove/v1', params={'token': users[1]['token'], 'u_id': 7896})
+    assert resp.status_code == 400
 
-def test_u_id_only_global_owner():
-    pass
+def test_u_id_only_global_owner(setup):
+    users = setup
+    resp = requests.delete(config.url + 'admin/user/remove/v1', params={'token': users[0]['token'], 'u_id': users[0]['auth_user_id']})
+    assert resp.status_code == 400
 
-def test_valid_removal():
-    pass
+def test_valid_removal(setup):
+    users = setup
+    resp = requests.delete(config.url + 'admin/user/remove/v1', params={'token': users[0]['token'], 'u_id': users[1]['auth_user_id']})
+    assert resp.status_code == 200
