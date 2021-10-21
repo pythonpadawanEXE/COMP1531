ad847f425167342d5d9c94014092c3129a63e47a
z5360175 <caleb.watts@student.unsw.edu.au>
2021-10-21 16:33:32 +1100

    Created stubs for admin/userpermission/change/v1 tests in admin_userpermission_change_v1_test.py

---
+++ tests/admin_userpermission_change_v1.py
@@ -0,0 +1,24 @@
+# admin_userpermission_change_v1_test.py
+# pytest file to test the implementation of admin/userpermission/change/v1
+import pytest
+import requests
+import json
+from src import config
+
+def test_u_id_invalid():
+    pass
+
+def test_attempt_to_demote_only_global_owner():
+    pass
+
+def test_invalid_permission_id():
+    pass
+
+def test_auth_user_not_global_owner():
+    pass
+
+def test_valid_demote():
+    pass
+
+def test_valid_promote():
+    pass
