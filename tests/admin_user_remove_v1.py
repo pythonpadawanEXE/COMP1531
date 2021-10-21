f199b967f452b9d6431f7bc86dcfdee6642f9d20
z5360175 <caleb.watts@student.unsw.edu.au>
2021-10-21 14:07:00 +1100

    Created stubs for tests in admin_user_remove_v1_test.py

---
+++ tests/admin_user_remove_v1.py
@@ -0,0 +1,18 @@
+# admin_user_remove_v1_test.py
+# pytest file to test the implementation of admin_user_remove_v1
+import pytest
+import requests
+import json
+from src import config
+
+def test_user_not_global_owner():
+    pass
+
+def test_u_id_not_valid():
+    pass
+
+def test_u_id_only_global_owner():
+    pass
+
+def test_valid_removal():
+    pass
