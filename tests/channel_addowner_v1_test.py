8b976ba17e8c8b362005fcf0de9c1ceed236677a
Mahi <starexify@gmail.com>
2021-10-22 21:51:48 +1100

    Blackbox Tests for channel_addowner_v1

--- tests/channel_addowner_v1_test.py
+++ tests/channel_addowner_v1_test.py
@@ -92,7 +92,7 @@ def channel_addowner(token, channel_id, u_id):
     response_data = response.json()
     return response_data
 
-# Make user with user id u_id an owner of the channel
+# Make user with user with id u_id an owner of the channel
 def make_u_id_channel_owner():
     # Create user1
     user_1 = register_user("js@email.com", "ABCDEFGH", "John", "Smith")
@@ -120,20 +120,140 @@ def make_u_id_channel_owner():
 
 # channel_id does not refer to a valid channel
 def test_invalid_channel_id():
-    pass
+    # Create user1
+    user_1 = register_user("js@email.com", "ABCDEFGH", "John", "Smith")
+    token_1 = user_1['token']
+
+    # Create user2
+    user_2 = register_user("jems@email.com", "ABCDEFGH", "Jemma", "Smith")
+    token_2 = user_2['token']
+    auth_user_id_2 = user_2['auth_user_id']
+
+    # user1 creates a channel
+    channel_id = channels_create(token_1, "Chan 1", True)['channel_id']
+
+    # user2 joins user1's channel
+    channel_join(token_2, channel_id)
+
+    # user1 promotes user2 as channel owner with invalid channel_id
+    response = requests.post(f"{BASE_URL}channel/addowner/v1", json={
+        'token' : token_1,
+        'channel_id' : 9999,
+        'u_id' : auth_user_id_2,
+    })
+    assert response.status_code == 400
 
 # u_id does not refer to a valid user
 def test_invalid_u_id():
-    pass
+    # Create user1
+    user_1 = register_user("js@email.com", "ABCDEFGH", "John", "Smith")
+    token_1 = user_1['token']
+
+    # Create user2
+    user_2 = register_user("jems@email.com", "ABCDEFGH", "Jemma", "Smith")
+    token_2 = user_2['token']
+    auth_user_id_2 = user_2['auth_user_id']
+
+    # user1 creates a channel
+    channel_id = channels_create(token_1, "Chan 1", True)['channel_id']
+
+    # user2 joins user1's channel
+    channel_join(token_2, channel_id)
+
+    # user1 promotes user2 as channel owner with invalid channel_id
+    response = requests.post(f"{BASE_URL}channel/addowner/v1", json={
+        'token' : token_1,
+        'channel_id' : channel_id,
+        'u_id' : 35,
+    })
+    assert response.status_code == 400
 
 # u_id refers to a user who is not a member of the channel
 def test_u_id_not_member_channel():
-    pass
+    # Create user1
+    user_1 = register_user("js@email.com", "ABCDEFGH", "John", "Smith")
+    token_1 = user_1['token']
+
+    # Create user2
+    user_2 = register_user("jems@email.com", "ABCDEFGH", "Jemma", "Smith")
+    token_2 = user_2['token']
+    auth_user_id_2 = user_2['auth_user_id']
+
+    # Create user3
+    user_3 = register_user("mike@email.com", "ABCDEFGH", "Mike", "Smith")
+    token_3 = user_3['token']
+    auth_user_id_3 = user_3['auth_user_id']
+
+    # user1 creates a channel
+    channel_id = channels_create(token_1, "Chan 1", True)['channel_id']
+
+    # user2 joins user1's channel
+    channel_join(token_2, channel_id)
+
+    # user1 promotes user3 as channel owner
+    response = requests.post(f"{BASE_URL}channel/addowner/v1", json={
+        'token' : token_1,
+        'channel_id' : channel_id,
+        'u_id' : auth_user_id_3,
+    })
+    assert response.status_code == 400
 
 # u_id refers to a user who is already an owner of the channel
 def test_u_id_already_owner():
-    pass
+    # Create user1
+    user_1 = register_user("js@email.com", "ABCDEFGH", "John", "Smith")
+    token_1 = user_1['token']
+
+    # Create user2
+    user_2 = register_user("jems@email.com", "ABCDEFGH", "Jemma", "Smith")
+    token_2 = user_2['token']
+    auth_user_id_2 = user_2['auth_user_id']
+
+    # user1 creates a channel
+    channel_id = channels_create(token_1, "Chan 1", True)['channel_id']
+
+    # user2 joins user1's channel
+    channel_join(token_2, channel_id)
+
+    # user1 promotes user2 as channel owner
+    channel_addowner(token_1, channel_id, auth_user_id_2)
+
+    # get channel details
+    details = channel_details(token_1, channel_id)
+
+    # check if user2 is an owner of channel_id
+    assert(details['owner_members'][1]['u_id'] == auth_user_id_2)
+
+    # user1 promotes user2 as channel owner again
+    response = requests.post(f"{BASE_URL}channel/addowner/v1", json={
+        'token' : token_1,
+        'channel_id' : channel_id,
+        'u_id' : auth_user_id_2,
+    })
+    assert response.status_code == 400
 
 # channel_id is valid and the authorised user (token) does not have owner permissions in the channel
 def test_token_is_not_channel_owner():
-    pass
+    # Create user1
+    user_1 = register_user("js@email.com", "ABCDEFGH", "John", "Smith")
+    token_1 = user_1['token']
+    auth_user_id_1 = user_1['auth_user_id']
+
+    # Create user2
+    user_2 = register_user("jems@email.com", "ABCDEFGH", "Jemma", "Smith")
+    token_2 = user_2['token']
+    auth_user_id_2 = user_2['auth_user_id']
+
+    # user1 creates a channel
+    channel_id = channels_create(token_1, "Chan 1", True)['channel_id']
+
+    # user2 joins user1's channel
+    channel_join(token_2, channel_id)
+
+    # user2 promotes user1 as channel owner again
+    response = requests.post(f"{BASE_URL}channel/addowner/v1", json={
+        'token' : token_2,
+        'channel_id' : channel_id,
+        'u_id' : auth_user_id_1,
+    })
+    assert response.status_code == 403
