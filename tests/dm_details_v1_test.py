6cf7c4b1e638daf215704f408fc3a46d0425bda7
Vincent <z5313503@ad.unsw.edu.au>
2021-10-24 00:13:51 +0800

    improve other.py coverage in helper functions for dm

--- tests/dm_details_v1_test.py
+++ tests/dm_details_v1_test.py
@@ -108,21 +108,19 @@ def test_unauthoried_user():
 
 def test_multi_dm_details():
     # New users
-    creator1 = register_user("js@email.com", "js123!@#", "John", "Smith")
+    creator = register_user("js@email.com", "js123!@#", "John", "Smith")
     member1 = register_user("lw@email.com", "lw123!@#", "Lewis", "Hamilton")
-    token1 = creator1['token']
+    token1 = creator['token']
     u_ids1 = [member1['auth_user_id']]
-    creator2 = register_user("mv@email.com", "mv123!@#", "Max", "Verstappen")
     member2 = register_user("sp@email.com", "sp123!@#", "Sergio", "Perez")
-    token2 = creator2['token']
     u_ids2 = [member2['auth_user_id']]
     # Dm ID
     dm_create(token1, u_ids1)['dm_id']
-    dm_id2 = dm_create(token2, u_ids2)['dm_id']
+    dm_id2 = dm_create(token1, u_ids2)['dm_id']
     # Dm details
-    details = dm_details(token2, dm_id2)
+    details = dm_details(token1, dm_id2)
     # User_ids
-    auth_user_id_list = [creator2['auth_user_id'], member2['auth_user_id']]
+    auth_user_id_list = [creator['auth_user_id'], member2['auth_user_id']]
     # Loop through the dm details and find if auth_user_id is in all_members
     for user in details['all_members']:
         assert(user['u_id'] in auth_user_id_list)
