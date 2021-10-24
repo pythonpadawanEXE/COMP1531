f1813dd8f21c71ef2d92110374fa8240f93ac718
Vincent <z5313503@ad.unsw.edu.au>
2021-10-24 17:37:19 +0800

    improved tests commenting

--- tests/dm_create_v1_test.py
+++ tests/dm_create_v1_test.py
@@ -1,3 +1,7 @@
+'''
+dm_create_v1_test
+'''
+
 import pytest
 import requests
 import json
@@ -6,51 +10,57 @@ from src import config
 @pytest.fixture
 def setup():
     '''
-    Clears the server data and creates a user
+    Clears the server data and creates 3 users for dm_create_setup
     '''
     requests.delete(config.url + 'clear/v1')
-    resp1 = requests.post(config.url + f"auth/register/v2",json={
+    user1 = requests.post(config.url + f"auth/register/v2",json={
         'email' : 'he@gmail.com',
         'password' : 'he123!@#',
         'name_first' : 'Hayden',
         'name_last' : 'Everest'
     })
-    creator = json.loads(resp1.text)
+    creator = json.loads(user1.text)
     
-    resp2 = requests.post(config.url + f"auth/register/v2",json={
+    user2 = requests.post(config.url + f"auth/register/v2",json={
         'email' : 'lw@gmail.com',
         'password' : 'lh123!@#',
         'name_first' : 'Lewis',
         'name_last' : 'Hamilton'
     })
-    user1 = json.loads(resp2.text)
-    u_ids = [user1['auth_user_id']]
+    member1 = json.loads(user2.text)
+    u_ids = [member1['auth_user_id']]
 
-    resp3 = requests.post(config.url + f"auth/register/v2",json={
+    user3 = requests.post(config.url + f"auth/register/v2",json={
         'email' : 'cl@gmail.com',
         'password' : 'cl123!@#',
         'name_first' : 'Charles',
         'name_last' : 'Leclerc'
     })
-    user2 = json.loads(resp3.text)
-    u_ids.append(user2['auth_user_id'])
+    member2 = json.loads(user3.text)
+    u_ids.append(member2['auth_user_id'])
 
     return (creator, u_ids)
 
 def test_dms_valid_create(setup):
     '''
-    A simple test to check a valid channel creation
+    A simple test to check a valid dm creation
     '''
     creator, u_ids = setup
     dm = requests.post(config.url + 'dm/create/v1', json={'token': creator['token'], 'u_ids' : u_ids})
     assert json.loads(dm.text) == {'dm_id': 1}
 
-def test_bad_token(setup):
+def test_invalid_token(setup):
+    '''
+    A simple test to check a invalid dm creation for invaid token pass in
+    '''
     u_ids = setup[1]
     dm = requests.post(config.url + 'dm/create/v1', json={'token': '', 'u_ids' : u_ids})
     assert dm.status_code == 403
 
-def test_bad_uids(setup):
+def test_invalid_uids(setup):
+    '''
+    A simple test to check a invalid dm creation for invaid uids(unregisted user) pass in
+    '''
     creator = setup[0]
     dm = requests.post(config.url + 'dm/create/v1', json={'token': creator['token'], 'u_ids' : [3,4]})
     assert dm.status_code == 400
