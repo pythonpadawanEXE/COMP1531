39fe92364654e30f811e9c8aec292280eec55d46
z5360175 <caleb.watts@student.unsw.edu.au>
2021-11-09 23:49:18 +1100

    created standup stubs in standup.py

---
+++ src/standup.py
@@ -0,0 +1,20 @@
+"""
+standup.py
+
+This module handles standup related functionality for the streams web app.
+
+Functions:
+    standup_start_v1() -> { time_finish }
+    standup_send_v1()
+    standup_active_v1()
+
+"""
+
+def standup_start_v1():
+    pass
+
+def standup_send_v1():
+    pass
+
+def standup_active_v1():
+    pass
