9ad0d94a51cd264c3b2c3d3d92ed996014d69ea0
Vincent <z5313503@ad.unsw.edu.au>
2021-11-11 16:33:25 +0800

    modified users_stats in

--- src/users.py
+++ src/users.py
@@ -8,7 +8,7 @@ Functions:
 """
 
 from src.data_store import data_store
-
+from src.other import check_valid_token, get_utilization_rate
 def users_all_v1():
     """ Returns a list of all users and their associated details.
 
@@ -31,3 +31,17 @@ def users_all_v1():
         })
 
     return users
+
+def users_stats_v1(token):
+    check_valid_token(token)
+    store = data_store.get()
+    workspace_stats = store['workspace_stats']
+    utilization_rate = get_utilization_rate
+    
+    return {'channels_exist': workspace_stats['channels_exist'],
+            'dms_exist': workspace_stats['dms_exist'],
+            'messages_sent': workspace_stats['messages_exist'],
+            'utilization_rate': utilization_rate
+
+    }
+    
